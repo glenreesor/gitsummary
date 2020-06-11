f6f5fadc6fb6c28e4f079d4c93aab7717dfae8eb
Glen Reesor <glen@wiseoldtree.ca>
2020-06-10 18:13:29 -0600

    Add copyright statements to new files

--- doc/tools/scriptedDemo.sh
+++ doc/tools/scriptedDemo.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Copyright 2020 Glen Reesor
+#
+# This file is part of gitsummary.
+#
+# Gitsummary is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Simple script to demonstrate `gmon` in action
 
 # Whether to use gitsummaryShellHelper.sh to create the fake shell prompt
